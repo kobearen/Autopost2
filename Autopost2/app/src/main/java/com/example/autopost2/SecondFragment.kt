362bbf5081af0bb1723d6538086102456f43ec06
satoshi.kimura <satoshi.kimura@i-freek.co.jp>
2019-05-27 21:21:18 +0900

    ダイアログ出ん

--- Autopost2/app/src/main/java/com/example/autopost2/SecondFragment.kt
+++ Autopost2/app/src/main/java/com/example/autopost2/SecondFragment.kt
@@ -11,16 +11,25 @@ import kotlinx.android.synthetic.main.fragment_second.view.*
 class SecondFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_second, container, false)
-        view.button?.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_second_to_third)
-        }
+//        view.tag_button?.setOnClickListener {
+//            Navigation.findNavController(it).navigate(R.id.action_second_to_third)
+//        }
         // 0517 新しくボタンから遷移する
         // navigation_graph.xmlにもactionを追加する必要がある
         view.back_button?.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_second_to_first)
         }
+
+        // ＃登録ダイアログを表示させる
+        // 0526 フラグメントと同じ感じでやってみる
+        view.tag_button?.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_second_to_tag)
+        }
+        // 仮ボタン
+        view.button?.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_second_to_third)
+        }
         return view
     }
 
 }
-
