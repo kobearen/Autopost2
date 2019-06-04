87ca741d0d277ddc073bdc5e26f305ad1eaa34de
satoshi.kimura <satoshi.kimura@i-freek.co.jp>
2019-06-04 10:21:16 +0900

    エディットテキストのテキストを取得

--- Autopost2/app/src/main/java/com/example/autopost2/MainActivity.kt
+++ Autopost2/app/src/main/java/com/example/autopost2/MainActivity.kt
@@ -11,12 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button?.setOnClickListener {
-            // エディットテキストのテキストを取得
-            if(edit_text.text != null){
-                // 取得したテキストを TextView に張り付ける
-                text_view.text = edit_text.text.toString()
-            }
-        }
     }
 }
