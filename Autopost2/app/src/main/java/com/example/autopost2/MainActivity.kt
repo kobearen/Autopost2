bfc046b229ee9a3a6bba5fa0cad718e273a1b2fc
satoshi.kimura <satoshi.kimura@i-freek.co.jp>
2019-06-29 21:37:00 +0900

    API

--- Autopost2/app/src/main/java/com/example/autopost2/MainActivity.kt
+++ Autopost2/app/src/main/java/com/example/autopost2/MainActivity.kt
@@ -8,12 +8,21 @@ import kotlinx.android.synthetic.main.fragment_second.view.*
 import android.content.Intent
 import android.widget.Toast
 import com.twitter.sdk.android.core.TwitterCore
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        postbutton.setOnClickListener {
+            // エディットテキストのテキストを取得
+            if(edit_text.text != null){
+                // 取得したテキストを TextView に張り付ける
+                text_view.text = edit_text.text.toString()
+            }
+        }
 
         // Twitter ログイン画面
 //        if (TwitterCore.getInstance().getSessionManager().getActiveSession() == null) {
