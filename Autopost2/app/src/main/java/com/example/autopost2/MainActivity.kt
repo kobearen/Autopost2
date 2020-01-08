13b3816207ba38505d966692167cacdcd1765d26
kobearen <kobearen@yahoo.co.jp>
2020-01-08 20:46:40 +0900

    Initial Commit

--- Autopost2/app/src/main/java/com/example/autopost2/MainActivity.kt
+++ Autopost2/app/src/main/java/com/example/autopost2/MainActivity.kt
@@ -6,25 +6,67 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_confirmation.*
 import kotlinx.android.synthetic.main.fragment_second.view.*
 import android.content.Intent
+import android.view.MenuItem
 import android.widget.Toast
 import com.twitter.sdk.android.core.TwitterCore
 import kotlinx.android.synthetic.main.fragment_first.*
 
 class MainActivity : AppCompatActivity() {
 
+    var memoryFlg: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        postbutton.setOnClickListener {
-            // エディットテキストのテキストを取得
-            if(edit_text.text != null){
-                // 取得したテキストを TextView に張り付ける
-                text_view.text = edit_text.text.toString()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "post"
+
+        postButton.setOnClickListener {
+            // 何も無い時
+            if(memoryFlg == 0){
+                memoryView.text = EditText.text
+                EditText.text = null
+                memoryFlg = memoryFlg + 1
+            }
+            // 1だけ埋まっている時
+            else if(memoryFlg == 1){
+                memoryView2.text = memoryView.text
+                memoryView.text = EditText.text
+                EditText.text = null
+                memoryFlg = memoryFlg + 1
+            }
+            // 全部埋まっている時
+            else if(memoryFlg == 2){
+                // memoryView2 が消えますが良いですか？ダイアログ
+                memoryView2.text = memoryView.text
+                memoryView.text = EditText.text
+                EditText.text = null
+                memoryFlg = 2
+            }
+            else if(memoryFlg >= 3){
+                //例外エラー
             }
         }
 
-        // Twitter ログイン画面
+        clearButton.setOnClickListener {
+            EditText.text = null
+            memoryView.text = "memory1"
+            memoryView2.text = "memory2"
+            memoryFlg = 0
+        }
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+}
+
+// Twitter ログイン画面
 //        if (TwitterCore.getInstance().getSessionManager().getActiveSession() == null) {
 //            val intent = Intent(this, LoginActivity::class.java)
 //            startActivity(intent)
@@ -33,5 +75,5 @@ class MainActivity : AppCompatActivity() {
 //            toast.show()
 //        }
 
-    }
-}
+//    }
+//}
